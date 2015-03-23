950c474b56d06da7f9188f567821a2bbfc13f8ce
Alex <alexandrajmh@gmail.com>
2015-03-22 21:16:00 -0500

    created file structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MappedArticles::Application.config.secret_token = 'aa3d6d1a86546b65b396022a198d335115896fd16f4587801cf04bdcdd4781c2503d5184ec3c535d401832a97c29a949ce95ad6a9c79d27946ff996cdecd6a7d'
