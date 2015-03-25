ab621c001c219976c576a0e46af286b1cc5e94c6
Alex <alexandrajmh@gmail.com>
2015-03-25 13:39:16 -0500

    Sumeet helped build out multiple article habtm locations assignment for creation form

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,8 @@ class Location < ActiveRecord::Base
   attr_accessible :alias, :address
   
   has_and_belongs_to_many :articles
+  
+  def custom_alias
+    self.alias # TODO - Whatever Alex wants
+  end
 end
