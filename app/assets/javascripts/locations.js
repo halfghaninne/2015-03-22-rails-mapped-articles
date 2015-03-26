8316e6a5fcefd3eb484df660ce4d09e6ef8b74f0
Alex <alexandrajmh@gmail.com>
2015-03-26 13:55:54 -0500

    added dotenv gem, but may not end up using it at all. mapbox seems to prefer requiring the access key in js

--- app/assets/javascripts/locations.js
+++ app/assets/javascripts/locations.js
@@ -2,10 +2,13 @@ document.addEventListener("DOMContentLoaded", function() {
 	
 	var map;
 	
-	map = L.mapbox.map('map', 'halfghaninne.liga61p0').setView([41.229, -96.032], 12)
+	map = L.mapbox.map('map', 'halfghaninne.liga61p0', {
+		accessToken: 'pk.eyJ1IjoiaGFsZmdoYW5pbm5lIiwiYSI6ImJuM0wwbm8ifQ.109o-AfmKCbMITcnOvrmFA'
+	});
+	
+	map.setView([41.229, -96.032], 12);
 	 
-	 
-	var get_locations = newXMLHttpRequest();
+	var get_locations = new XMLHttpRequest();
 	get_locations.open("get", "locations", true);
 	get_locations.send();
 	get_locations.addEventListener("load", function() {
