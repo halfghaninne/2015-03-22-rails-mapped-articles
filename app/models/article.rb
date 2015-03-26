63191ff36403bbf302af8bc13fd9ecf98c874c98
Alex <alexandrajmh@gmail.com>
2015-03-25 20:54:24 -0500

    location assignment not working properly. committing changes but abandoning feature for now.  work on map embed first, THEN work on edit functionality ie destroying associations in join table, making sure that drop downs in edit form work properly.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,12 +1,7 @@
 class Article < ActiveRecord::Base
-  attr_accessible :title, :body, :author_id, :published, :location_ids
+  attr_accessible :title, :body, :author_id, :published, :location_ids, :locations
   
   belongs_to :author
   has_and_belongs_to_many :locations
   
-  # def location_ids=(array_of_ids_of_locations)
-  #   array_of_ids_of_locations.each do |a|
-  #     self.locations << a # Adds a row to the bridge table.
-  #   end
-  # end
 end
