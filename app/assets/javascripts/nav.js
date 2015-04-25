e2fe89f48026d4120782d14a53a018bce8370004
Alexandra Millatmal <alexandrajmh@gmail.com>
2015-04-24 22:05:46 -0500

    builds mobile slide out nav for main page

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -10,11 +10,26 @@ window.addEventListener("load", function(){
         mobileNav.style.display = "none";
       }
       
-      mobileNav.classList.toggle("mobile_show_menu_is_shown");
+      mobileNav.classList.toggle("mobile_menu_is_shown");
       mobileNav.classList.toggle("is-mobile-nav-shown");
       
     });
   }
-  
-  
+});
+
+window.addEventListener("load", function(){
+  if (document.getElementById("mobile_nav_menu")) {
+    document.getElementById("mobile_nav_menu").addEventListener("click", function(){
+      var mobileMainNav = document.getElementById("mobile_main_menu");
+      
+      if (mobileMainNav.style.display == "none") {
+        mobileMainNav.style.display = "block";
+      } else {
+        mobileMainNav.style.display = "none";
+      }
+      
+      mobileMainNav.classList.toggle("mobile_menu_is_shown");
+      mobileMainNav.classList.toggle("is-mobile-nav-shown")
+    });
+  }
 });
