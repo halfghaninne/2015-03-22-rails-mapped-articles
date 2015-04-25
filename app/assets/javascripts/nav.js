f996bcf4bfb02948a28eb81459ced0c8d90ef18d
Alexandra Millatmal <alexandrajmh@gmail.com>
2015-04-24 20:17:36 -0500

    mobile nav slide out working for article show page. next step is to replicate these steps for the main page.

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -3,19 +3,16 @@ window.addEventListener("load", function(){
   if (document.getElementById("mobile_nav_show_menu")) {
     document.getElementById("mobile_nav_show_menu").addEventListener("click", function(){
       var mobileNav = document.getElementById("mobile_show_menu");
-      var pageContainer = document.getElementById("page_container");
+      
+      if (mobileNav.style.display == "none") { 
+        mobileNav.style.display = "block";
+      } else {
+        mobileNav.style.display = "none";
+      }
       
       mobileNav.classList.toggle("mobile_show_menu_is_shown");
       mobileNav.classList.toggle("is-mobile-nav-shown");
       
-      pageContainer.classList.toggle("is-mobile-nav-shown");
-      
-      
-      // if (document.getElementById("mobile_show_menu").style.display == "none") {
-//         document.getElementById("mobile_show_menu").style.display = "block";
-//       } else {
-//         document.getElementById("mobile_show_menu").style.display = "none";
-//       }
     });
   }
   
