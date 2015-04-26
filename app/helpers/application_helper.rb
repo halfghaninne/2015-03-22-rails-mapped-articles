9953b73a603dbb41b17e4fd88e70b93562917e96
Alexandra Millatmal <alexandrajmh@gmail.com>
2015-04-26 15:05:03 -0500

    modifies styling for new article page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
     x = render "shared/locations_form", f: f, newPin: newPin, 
     ruby_object_id: ruby_object_id
     
-    link_to("Add another location", "#", :class => "add_location_link", style: "color: red", data: {fields: x.gsub("\n", ""), rubyid: ruby_object_id})
+    link_to("Add another new location", "#", :class => "add_location_link", style: "color: red", data: {fields: x.gsub("\n", ""), rubyid: ruby_object_id})
   end
   
   def link_to_add_dropdown(article, f)
