a391ab7fd054b6158f3e85a536faca7aadbbdb10
Alexandra Millatmal <alexandrajmh@gmail.com>
2015-05-04 17:07:00 -0500

    adds user check validation in application and location controllers to solve issues with menu display

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  before_filter :validate_user_presence
+  
+  def validate_user_presence
+    if session[:user_id] != nil
+      @user = User.find(session[:user_id])
+      @admin = true if @user.is_admin
+    end
+  end
+  
   def about
     render "/about"
   end
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,14 @@
 class LocationsController < ApplicationController
   
+  before_filter :validate_user_presence
+  
+  def validate_user_presence
+    if session[:user_id] != nil
+      @user = User.find(session[:user_id])
+      @admin = true if @user.is_admin
+    end
+  end
+  
   def show
     @location = Location.find(params[:id]) 
     #move the following out into a location method, once functional
